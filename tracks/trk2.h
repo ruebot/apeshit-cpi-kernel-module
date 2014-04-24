32111df5f5d34906a939764f0220096cf95d6d6d
nruest <ruestn@gmail.com>
2014-04-23 21:26:38 -0400

    Add two more tracks.

---
+++ tracks/trk2.h
@@ -0,0 +1,6 @@
+#define APESHIT_CPI_TRK2_LEN 200680000
+static char apeshit_cpi_trk2[] = {
+
+#include <trk1data.h>
+
+};
---
+++ tracks/trk3.h
@@ -0,0 +1,6 @@
+#define APESHIT_CPI_TRK3_LEN 160760000
+static char apeshit_cpi_trk3[] = {
+
+#include <trk1data.h>
+
+};
